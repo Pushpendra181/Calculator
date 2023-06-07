910b32e8d3fc0726f2e9d945dc6a74320be14220
Pushpendra181 <123919866+Pushpendra181@users.noreply.github.com>
2023-06-07 15:01:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let inpute = document.getElementById("input_box");
+let buttons = document.querySelectorAll('button')
+
+let string = "";
+let arr = Array.from(buttons)
+arr.forEach(button => {
+    button.addEventListener('click',(e) => {
+        if (e.target.innerHTML == '='){
+            string = eval(string);
+            inpute.value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            inpute.value = string;
+        }
+        else if (e.target.innerHTML == "DEL"){
+            string = string.substring(0, string.length-1);
+            inpute.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            inpute.value = string;
+        }
+        
+    })
+})
